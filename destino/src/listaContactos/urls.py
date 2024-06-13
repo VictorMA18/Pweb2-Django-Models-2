92ba5a702f8ed50f4075f8b0ad56849da3725cd7
VictorMA18 <vmamanian@unsa.edu.pe>
2024-06-13 00:13:51 -0500

    Registrando el url de la nueva pagina de inicio para que cuando entremos con la ruta ip nos mande a un hola mundo desde django

--- destino/src/listaContactos/urls.py
+++ destino/src/listaContactos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import myHomeView
 
 urlpatterns = [
+    path('', myHomeView, name='Página de Inicio'),
     path('admin/', admin.site.urls),
 ]
